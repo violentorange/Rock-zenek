005865d8a8eb2e64335acbb9e0692b3958ddff54
Lőrincz Gergely Tamás <lorincz.gergely.tamas@students.jedlik.eu>
2024-06-18 13:09:00 +0200

    Saving incase it might be needed

---
+++ src/views/LoginView backup.vue
@@ -0,0 +1,27 @@
+<template>
+ 
+  <div class="bg-dark text-white">
+
+    <div class="container p-5">
+    <form class="row g-3">
+
+      <div class="col-md-12">
+        <label for="inputEmail4" class="form-label">Username</label>
+        <input required type="email" class="form-control" id="inputEmail4" />
+      </div>
+
+      <div class="col-md-12">
+        <label for="inputPassword4" class="form-label">Password</label>
+        <input required type="password" class="form-control" id="inputPassword4" />
+      </div>
+
+      <div class="col-12">
+        <button type="submit" style="background-color: darkred; border-color: darkred;" class="btn btn-danger">Log in</button>
+      </div>
+
+    </form>
+  </div>
+
+  </div>
+  </template>
+  
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -45,25 +45,24 @@ const login = () => {
 </script>
 
 <template>
-  <div class="login-page" p-2>
+  <div class="">
     <div class="form-container">
-      <h4 class="honk fs-1  p-0 m-0 " id="LoginText">BEJELENTKEZÉS</h4>
 
-    <div class="container w-25">
+    <div class="container bg-light rounded border border-5 border-white w-25" style="--bs-white-rgb: 139, 0, 0;">
       <form class="row g-3" @submit.prevent="login">
         <div class="col-md-12">
           <label for="inputEmail" class="form-label fs-5 text-light fw-bold">Email:</label>
           <input v-model="email" required type="email" class="form-control" id="inputEmail" autocomplete="email"
-            placeholder="Kérem ide írja be az email címét: pl 123@gmail.com">
+            placeholder="Adja meg az email címét: pl kala.pal@gmail.com">
         </div>
         <br>
         <div class="col-md-12">
           <label for="inputPassword" class="form-label fs-5 text-light fw-bold">Jelszó:</label>
           <input v-model="password" required type="password" class="form-control" id="inputPassword" autocomplete="current-password"
-            placeholder="Kérem írja be a jelszavát!">
+            placeholder="Adja meg a jelszavát!">
         </div>
         <div class="col-12 p-3">
-          <button type="submit" class="honk  login-button fs-5 btn btn-dark">Bejelentkezés</button>
+          <button type="submit" class="btn btn-dark darkred">Bejelentkezés</button>
         </div>
       </form>
       <div class="success" v-if="successMessage">{{ successMessage }}</div>
@@ -74,95 +73,6 @@ const login = () => {
 </template>
 
 <style scoped>
-/* csak erre az oldalra vonatkozik az itt megadott stlye ha scopedre van állítva */
-/* Add any necessary styling here */
-
-
-
-#LoginText {
-  text-align: center;
-  margin-bottom: 20px;
-  /* background-color: transparent;  */
-  /* font-family:Arial, Helvetica, sans-serif;
-  font-weight: 500; */
-
-}
-
-
-input {
-  border-radius: 11px;
-  color: white;
-  background-color: antiquewhite, 0.1;
-  border: 0.1px solid rgba(142, 212, 212, 0.5);
-  box-shadow: 3px 3px 9px rgba(0, 0, 0, 0.9);
-  border-width: 4px 1.25em;
-  border-style: solid;
-}
-
-.login-button {
-  border-radius: 11px;
-  background-color: black;
-  opacity: 0.8;
-  box-shadow: 3px 3px 9px rgba(0, 0, 0, 0.9);
-  
-}
-
-.form-container {
-  background-color: transparent; 
-  /* Átlátszó háttérszín */
-  opacity: 94%;
-  width: 100vw;
-}
-
-.form-control {
-  background-color: gray;
-  opacity: 0.9;
-}
-
-.login-page {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  min-height: 100vh;
-  background-image: url('@/assets\css\images\space satellite.png');
-  /* C:\Users\tomi2\Legovizsga-main-main\Legovizsga-main\src\assets\css\images\space satellite.png */
-  background-size: cover;
-  background-position: center;
-  position: relative;
-  z-index: 1;
-}
-
-/* .form-container {
-  background-color: rgba(255, 255, 255, 0.8);  */
-  /* Fehér háttér, hogy kiemelje a formot */
-  /* padding: 20px;
-  border-radius: 10px;
-  z-index: 2;
-  position: relative;
-} */
-
-.success {
-  color: rgb(45, 216, 45);
-  height: 150px;
-  text-size-adjust: 100%;
-  font-size: 125px;
-  font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
-  font-weight: 300;
-}
-
-.error {
-  color: rgb(247, 4, 4);
-  text-size-adjust: 100%;
-  font-size: 45rem;
-  font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
-  font-weight: 300;
-}
-
-.success,
-.error {
-  padding-bottom: 10px; 
-  font-size: 18px; 
-}
 
 </style>
 
